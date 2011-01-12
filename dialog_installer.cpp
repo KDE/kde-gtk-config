5ffa8b2d047d262608357d77f1786500f4ae4abc
Manuel Tortosa <manutortosa@chakra-project.org>
2011-01-12 02:26:14 +0100

    tr() -> i18n() and translations

--- dialog_installer.cpp
+++ dialog_installer.cpp
@@ -57,12 +57,12 @@ void DialogInstaller::selectThemeFile(){
 
     QString archivo = QFileDialog::getOpenFileName(
             this,
-            tr("Selecciona un archivo de temas gtk valido"),
+            i18n("Select a valid gtk theme file"),
             QDir::homePath(),
-            tr("Archivo Comprimido (*.tar.gz *.tar)")
+            i18n("Compressed file (*.tar.gz *.tar)")
             );
 
-    qDebug() << "TEMA GTK SELECCIONADO : " << archivo;
+    qDebug() << "SELECTED GTK THEME : " << archivo;
 
     // NO hay nada que hacer si no se selecciono un archivo
     if(archivo.isEmpty())
@@ -76,15 +76,15 @@ void DialogInstaller::selectThemeFile(){
 
 void DialogInstaller::selectIconThemeFile()
 {
-    qDebug() << "SELECCIONANDO TEMA DE ICONO";
+    qDebug() << "SELECTING ICONS THEME";
     QString archivo = QFileDialog::getOpenFileName(
             this,
-            tr("Selecciona un archivo de temas de iconos valido"),
+            i18n("Select a vaild icons theme file"),
             QDir::homePath(),
-            tr("Archivo Comprimido (*.tar.gz *.tar)")
+            i18n("Compressed file (*.tar.gz *.tar)")
             );
 
-    qDebug() << "TEMA DE ICONO SELECCIONADO : " << archivo;
+    qDebug() << "SELECTED ICONS THEME : " << archivo;
 
     // NO hay nada que hacer si no se selecciono un archivo
     if(archivo.isEmpty())
@@ -104,31 +104,31 @@ void DialogInstaller::installTheme()
 
     QString archivo = ui->txt_theme_file->text();
 
-    qDebug()<< "*************** INSTALACION DE TEMA GTK";
-    qDebug()<< "Archivo a instalar" << archivo;
+    qDebug()<< "*************** GTK THEME INSTALLATION";
+    qDebug()<< "File to install" << archivo;
 
     //Verificamos si existe algo en el campo de texto
     if(archivo.isEmpty()){
-        qDebug() << "ERROR: campo de texto vacio";
+        qDebug() << "ERROR: empty text field";
         return;
     }
 
     //Verificamos si el archivo es valido
     if(!archivo.contains(QRegExp("(.tar.gz|tar)"))){
-        qDebug() << "ERROR: El archivo no es valido";
+        qDebug() << "ERROR: Is not a valid file";
         return;
     }
 
     //Por si las dudas verifica si el archivo existe
     QFileInfo fichero(archivo);
     if(!fichero.exists()){
-        qDebug() << "ERROR: Ese fichero no existe";
+        qDebug() << "ERROR: File does not exist";
         return;
     }
 
     //Tambien por si las dudas verifica si es un directorio
     if(fichero.isDir()){
-        qDebug() << "ERROR: Lo que estas tratando de instalar es un directorio";
+        qDebug() << "ERROR: Trying to install a directory";
         return;
     }
 
@@ -143,31 +143,31 @@ void DialogInstaller::installThemeIcon()
 {
     QString archivo = ui->txt_icon_file->text();
 
-    qDebug()<< "*************** INSTALACION DE TEMA GTK";
-    qDebug()<< "Archivo a instalar" << archivo;
+    qDebug()<< "*************** GTK THEME INSTALLATION";
+    qDebug()<< "File to install" << archivo;
 
     //Verificamos si existe algo en el campo de texto
     if(archivo.isEmpty()){
-        qDebug() << "ERROR: campo de texto vacio";
+        qDebug() << "ERROR: empty text field";
         return;
     }
 
     //Verificamos si el archivo es valido
     if(!archivo.contains(QRegExp("(.tar.gz|tar)"))){
-        qDebug() << "ERROR: El archivo no es valido";
+        qDebug() << "ERROR: it is not a valid file";
         return;
     }
 
     //Por si las dudas verifica si el archivo existe
     QFileInfo fichero(archivo);
     if(!fichero.exists()){
-        qDebug() << "ERROR: Ese fichero no existe";
+        qDebug() << "ERROR: This fie does not exist";
         return;
     }
 
     //Tambien por si las dudas verifica si es un directorio
     if(fichero.isDir()){
-        qDebug() << "ERROR: Lo que estas tratando de instalar es un directorio";
+        qDebug() << "ERROR: You try to install a directory";
         return;
     }
 
@@ -183,7 +183,7 @@ void DialogInstaller::installThemeIcon()
 void DialogInstaller::themeAnalisys()
 {
 
-    ui->lb_theme_notice->setText(tr("Analizando tema ... "));
+    ui->lb_theme_notice->setText(i18n("Parsing theme ... "));
 
     //desabilitamos los controles
     ui->but_theme_filechoice->setEnabled(false);
@@ -205,11 +205,11 @@ void DialogInstaller::checkThemeAnalisys()
     ui->txt_theme_file->setEnabled(true);
 
     if(threadAnalisysTheme->isSuccess()){
-        ui->lb_theme_notice->setText(tr("El tema gtk se puede instalar"));
+        ui->lb_theme_notice->setText(i18n("This Gtk theme can be installed"));
         ui->but_theme_install->setEnabled(true);
     }
     else{
-        ui->lb_theme_notice->setText(tr("El tema gtk que se quiere instalar no es valido"));
+        ui->lb_theme_notice->setText(i18n("This Gtk theme cannot be installed"));
         //desahibilitamos el boton de instalar para que no se pueda instalar
         ui->but_theme_install->setEnabled(false);
     }
@@ -218,7 +218,7 @@ void DialogInstaller::checkThemeAnalisys()
 
 void DialogInstaller::themeIconAnalisys()
 {
-    ui->lb_icon_notice->setText(tr("Analizando tema ... "));
+    ui->lb_icon_notice->setText(i18n("Parsing theme ... "));
 
     //desabilitamos los controles
     ui->but_icon_filechoice->setEnabled(false);
@@ -239,11 +239,11 @@ void DialogInstaller::checkThemeIconAnalisys()
     ui->txt_icon_file->setEnabled(true);
 
     if(threadAnalisysThemeIcon->isSuccess()){
-        ui->lb_icon_notice->setText(tr("El tema de iconos se puede instalar"));
+        ui->lb_icon_notice->setText(i18n("This icons theme can be installed"));
         ui->but_icon_install->setEnabled(true);
     }
     else{
-        ui->lb_icon_notice->setText(tr("El tema de iconos que se quiere instalar no es valido"));
+        ui->lb_icon_notice->setText(i18n("This icons theme cannot be installed"));
         //desahibilitamos el boton de instalar para que no se pueda instalar
         ui->but_icon_install->setEnabled(false);
     }
@@ -267,14 +267,14 @@ void DialogInstaller::enableGUIThemeIconInstaller(){
 }
 
 void DialogInstaller::disableGUIThemeInstaller(){
-    ui->lb_theme_notice->setText("Instalando el tema Gtk ...");
+    ui->lb_theme_notice->setText("Installing Gtk theme ...");
     ui->but_theme_filechoice->setEnabled(false);
     ui->but_theme_install->setEnabled(false);
     ui->txt_theme_file->setEnabled(false);
     
 }
 void DialogInstaller::disableGUIThemeIconInstaller(){
-    ui->lb_icon_notice->setText("Instalando los iconos ...");
+    ui->lb_icon_notice->setText("Installing icons ...");
     ui->but_icon_install->setEnabled(false);
     ui->but_icon_filechoice->setEnabled(false);
     ui->txt_icon_file->setEnabled(false);
@@ -283,23 +283,23 @@ void DialogInstaller::disableGUIThemeIconInstaller(){
 
 void DialogInstaller::refreshGUIIconTheme(){
     if(threadForIcon->isSuccess()){
-        ui->lb_icon_notice->setText(tr("Iconos instalados Satisfactoriamente"));
-        ui->txt_icon_file->setText(tr(""));
+        ui->lb_icon_notice->setText(i18n("Icons Successfully Installed"));
+        ui->txt_icon_file->setText(i18n(""));
     }
     else{
-        ui->lb_icon_notice->setText(tr("No se Pudo instalar el tema de iconos"));
-        ui->txt_icon_file->setText(tr(""));
+        ui->lb_icon_notice->setText(i18n("The icons theme cannot be installed"));
+        ui->txt_icon_file->setText(i18n(""));
     }
 }
 
 // Esto se ejecuta cuando un tema es tratado a instalar
 void DialogInstaller::refreshGUITheme(){
     if(threadForTheme->isSuccess()){
-        ui->lb_theme_notice->setText(tr("Tema Gtk instalado Satifactoriamente"));
+        ui->lb_theme_notice->setText(i18n("Gtk Theme Sucessfully Installed"));
         ui->txt_theme_file->setText("");
     }
     else{
-        ui->lb_theme_notice->setText(tr("No se Pudo instalar el tema gtk"));
+        ui->lb_theme_notice->setText(i18n("The Gtk theme cannot be installed"));
         ui->txt_theme_file->setText("");
     }
 }
