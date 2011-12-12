2e36fa50c23d3b3087dbedfc655ae9723a4b881a
Aleix Pol <aleixpol@kde.org>
2011-12-12 02:57:51 +0100

    Installer class. Translate and refactoring

--- src/installer.cpp
+++ src/installer.cpp
@@ -1,72 +1,24 @@
 #include "installer.h"
 
-bool Installer::installTheme(QString &urlPackage)
+bool Installer::installTheme(const QString &urlPackage)
 {
-    QDir destino(QDir::homePath()+"/.themes");
-    qDebug() << "INSTALLING GTK THEME " << urlPackage << " IN "  << destino.path();
+    QString destino(QDir::homePath()+"/.themes");
+    QDir::home().mkpath(destino);
 
-    //Verifica primero si es que existe la carpetas donde se guardan los temas
-    if(!destino.exists()){
-        //Trata de crear la carpeta de los temas
-        qDebug() << "WARNING: The folder for saving the icons does no exist, tying to create it";
-        if(QProcess::execute("mkdir", QStringList(destino.path())) != 0){
-            qDebug() << "ERROR: Folder " << destino.path() << " cannot be created";
-            return false;
-        }
-
-        qDebug() << "Folder successfully created";
-
-    }
-
-
-    // ejecutaremos el siguiente comando: tar -xvf fichero -C destino
-
+    //TODO: Port to KArchive
     QStringList argumentos;
-    argumentos << "-xvf" << urlPackage << "-C" << destino.path();
-
-    /**
-      Si execute devuelve 0 significa que el comando se ejecuto satisfactoriamente
-      */
-    if(QProcess::execute("tar", argumentos) != 0){
-        return false;
-    }
-
-    return true;
+    argumentos << "-xvf" << urlPackage << "-C" << destino;
+    return QProcess::execute("tar", argumentos) == 0;
 }
 
-
-bool Installer::installIconTheme(QString &urlPackage)
+bool Installer::installIconTheme(const QString &urlPackage)
 {
 
-
-    QDir destino(QDir::homePath()+"/.icons");
-
-    //Verifica primero si es que existe la carpetas donde se guardan los temas
-    if(!destino.exists()){
-        //Trata de crear la carpeta de los temas
-        qDebug() << "WARNING: The folder for saving the icons does no exist, tying to create it";
-        if(QProcess::execute("mkdir", QStringList(destino.path())) != 0){
-            qDebug() << "ERROR: In folder " << destino.path() << " cannot be created";
-            return false;
-        }
-
-        qDebug() << "Folder successfully created";
-
-    }
-
-    qDebug() << "INSTALLING ICONS THEME"<< urlPackage << " IN : " << destino.path();
-
-    // Argumentos para el comando tar
+    QString destino(QDir::homePath()+"/.icons");
+    QDir::home().mkpath(destino);
+    
+    //TODO: Port to KArchive
     QStringList argumentos;
-    argumentos << "-xvf" << urlPackage << "-C" << destino.path();
-
-    /**
-      Si execute devuelve 0 significa que el comando se ejecuto satisfactoriamente
-      */
-    if(QProcess::execute("tar", argumentos) != 0){
-        return false;
-    }
-
-    return true;
-
+    argumentos << "-xvf" << urlPackage << "-C" << destino;
+    return QProcess::execute("tar", argumentos) == 0;
 }
--- src/installer.h
+++ src/installer.h
@@ -5,23 +5,16 @@ class Installer
 {
 public:
     /**
-      Este metodo se encarga de desempaquetar el archivo
-      que contiene el tema gtk2, en la carpeta donde se almacenan
-      los temas gtk2
-
-      Para desempaquetar utiliza el comando tar:
-      >> tar -xvf tema.tar.gz -C /home/usuario/.themes
+      This function will unpack the @p urlPackage GTK theme packages wherever
+      it has to be installed
       */
-    static bool installTheme(QString &urlPackage);
+    static bool installTheme(const QString& urlPackage);
 
     /**
-      Desempaqueta el archivo que contiene los ficheros del tema de iconos,
-      en la carpeta donde se almacenan los iconos
-
-      Para desempaquetar utiliza el comando tar:
-      >> tar -xvf tema.tar.gz -C /home/usuario/.icons
+      This function will unpack the @p urlPackage icons theme packages wherever
+      it has to be installed
       */
-    static bool installIconTheme(QString &urlPackage);
+    static bool installIconTheme(const QString& urlPackage);
 
 };
 
