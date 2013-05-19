a3309d4127f2cc631426098b2601afef285f3b1a
manu tortosa <manutortosa@gmail.com>
2013-05-19 11:30:45 +0200

    Switch indexes to correct values

--- src/appearancegtk3.cpp
+++ src/appearancegtk3.cpp
@@ -69,8 +69,8 @@ bool AppearanceGTK3::saveSettings(const QString& file)
     flow3 << "gtk-icon-theme-name="<< m_settings["icon"] << "\n";
     flow3 << "gtk-fallback-icon-theme=" << m_settings["icon_fallback"] << "\n";
     flow3 << "gtk-toolbar-style=" << m_settings["toolbar_style"] << "\n";
-    flow3 << "gtk-menu-images=" << m_settings["show_icons_buttons"] << "\n";
-    flow3 << "gtk-button-images=" << m_settings["show_icons_menus"] << "\n";
+    flow3 << "gtk-menu-images=" << m_settings["show_icons_menus"] << "\n";
+    flow3 << "gtk-button-images=" << m_settings["show_icons_buttons"] << "\n";
     
     return true;
 }
