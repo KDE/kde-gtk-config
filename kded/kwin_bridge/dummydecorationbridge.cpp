c76787c5798345b43c59bc31888970ce0eb991d7
Nicolas Fella <nicolas.fella@gmx.de>
2024-12-04 19:10:23 +0100

    Don't try to create buttons when the factory failed to load
    SENTRY: KDED-JD

--- kded/kwin_bridge/dummydecorationbridge.cpp
+++ kded/kwin_bridge/dummydecorationbridge.cpp
@@ -95,6 +95,10 @@ std::unique_ptr<KDecoration3::DecoratedWindowPrivate> DummyDecorationBridge::cre
 
 void DummyDecorationBridge::paintButton(QPainter &painter, const QString &buttonType, const QString &buttonState)
 {
+    if (!m_factory) {
+        return;
+    }
+
     disableAnimations();
     std::unique_ptr<KDecoration3::DecorationButton> button{
         m_factory->create<KDecoration3::DecorationButton>(m_decoration,
